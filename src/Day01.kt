76d759d14409b8d3c3f285175338ce9cdd9574de
Sebastian Heeschen <sebastian.heeschen@gmail.com>
2025-12-01 20:08:44 +0100

    Add Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,54 @@
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+private const val DIAL_SIZE = 100
+private const val DIAL_START = 50
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input
+        .map { splitLineToInstruction(it) }
+        .runningFold(DIAL_START) { dial, (direction, distance) ->
+            val value = when (direction) {
+                Direction.L -> dial - distance
+                Direction.R -> dial + distance
+            }
+            value.mod(DIAL_SIZE)
+        }
+        .count { it == 0 }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    fun part2(input: List<String>): Int = input
+        .map { splitLineToInstruction(it) }
+        .map { (direction, distance) ->
+            when (direction) {
+                Direction.L -> -1 * distance
+                Direction.R -> distance
+            }
+        }
+        .flatMap { distance -> List(distance.absoluteValue) { distance.sign } }
+        .runningFold(DIAL_START) { dial, direction ->
+            val i = dial + direction
+            (i.mod(DIAL_SIZE) + DIAL_SIZE).mod(DIAL_SIZE)
+        }
+        .count { it == 0 }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun splitLineToInstruction(line: String): Pair<Direction, Int> {
+    // line has format Lxx or Rxx
+    val direction = Direction.valueOf(line.first().toString())
+    val distance = line.drop(1).toInt()
+    return direction to distance
+}
+
+private enum class Direction {
+    L, R
 }
